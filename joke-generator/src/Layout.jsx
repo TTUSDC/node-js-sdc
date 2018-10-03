f6f44e41aa74b711689f5340dc69d4970ed15e03
Ynigo Reyes <ynigoreyes@gmail.com>
2018-10-03 14:53:31 -0500

    lint

--- joke-generator/src/Layout.jsx
+++ joke-generator/src/Layout.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
-import { red, black } from '@material-ui/core/colors'
-import { Provider } from 'react-redux'
-import store from 'redux/store'
-import DisplayContainer from 'components/Display/DisplayContainer.jsx'
-import HeaderContainer from 'components/Header/HeaderContainer.jsx'
-import SnackBarContainer from 'components/SnackBar/SnackBarContainer.jsx'
-import FooterContainer from 'components/Footer/FooterContainer.jsx'
-import { hot } from 'react-hot-loader'
+import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { red, black } from '@material-ui/core/colors';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+import DisplayContainer from 'components/Display/DisplayContainer.jsx';
+import HeaderContainer from 'components/Header/HeaderContainer.jsx';
+import SnackBarContainer from 'components/SnackBar/SnackBarContainer.jsx';
+import FooterContainer from 'components/Footer/FooterContainer.jsx';
+import { hot } from 'react-hot-loader';
 
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
     primary: black,
     secondary: red,
-  }
-})
+  },
+});
 
-console.log(process.env)
+console.log(process.env);
 
 class Layout extends React.Component {
   render() {
