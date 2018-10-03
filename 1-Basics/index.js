2e750678d5f4f1e0a08d7307882225b42448d148
Ynigo Reyes <ynigoreyes@gmail.com>
2018-10-03 17:20:44 -0500

    update

--- 1-Basics/index.js
+++ 1-Basics/index.js
@@ -1,5 +1,62 @@
-function main() {
-    console.log("Hello World")
+function hello() {
+    console.log("Hello World!")
 }
 
-main()
+let ice_cream = 'Vanilla'
+let favorite = `${ice_cream} is my favorite ice cream`
+
+let num = 18
+let almost_pi = 3.14465836875643875
+
+let falsy = false
+let truthy = true
+
+let x = 120
+
+let big_number = x > 100
+
+let person = {
+    name: 'Miggy',
+    age: 18,
+    interests: {
+        math: false,
+        food: true
+    }
+}
+
+let first = 10
+let second = 3
+
+function age_calc(current_year, birth_year) {
+    // ...
+}
+
+function inches_cm(inches) {
+    // ...
+    return "hello"
+}
+
+let obj = {
+    name: "Miggy",
+    age: 18,
+    catagory1: {
+        foo: "I like math",
+    },
+    catagory2: {
+        bar: 'I like Coding'
+    }
+}
+
+let array = ["Miggy", "Josh"]
+
+let element = array.pop()
+
+console.log(element)
+console.log(array)
+
+console.log(array.push("Baylee"))
+array.push("Matt")
+array.push("Brett")
+
+console.log(array)
+// 1in 2.54cm
