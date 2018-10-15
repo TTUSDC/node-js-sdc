4db6e2860639806b881d4d3550c65303a7251754
ynigoreyes <ynigoreyes@gmail.com>
2018-10-14 21:46:06 -0500

    added examples of all three different async patterns

--- 3-Async/axios_requests.js
+++ 3-Async/axios_requests.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const request = require('request')
 
 axios.get('http://jsonplaceholder.typicode.com/todos/1')
   .then(({ data }) => {
@@ -7,3 +8,21 @@ axios.get('http://jsonplaceholder.typicode.com/todos/1')
   .catch((err) => {
     console.error(err)
   })
+
+request.get('http://jsonplaceholder.typicode.com/todos/1', (err, data) => {
+  if (err) throw err
+  console.log(data.body)
+})
+
+function ApiCall() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const fetchedData = await axios.get('http://jsonplaceholder.typicode.com/todos/1')
+      resolve(fetchedData.data)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+ApiCall().then((todo) => console.log(todo) )
